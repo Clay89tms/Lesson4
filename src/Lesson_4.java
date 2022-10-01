7b25866c18af582db5dc57aefe2a92155b4e15c3
Clay89tms <bboyClay89@gmail.com>
2022-10-01 21:48:52 +0300

    Second

--- src/Lesson_4.java
+++ src/Lesson_4.java
@@ -1,29 +1,148 @@
+import java.nio.file.CopyOption;
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Lesson_4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Введите номер ДЗ 4: ");
+
 
         Integer dz4 = null;
-        if (sc.hasNextInt()) {
-            dz4 = sc.nextInt();
-            switch (dz4) {
-                case (0):
-                    dz04_00();
-                    break;
-                case (1):
-                    dz04_01();
-                    break;
-                case (2):
-                    dz04_02();
-                    break;
-                case (3):
-                    dz04_03();
+
+
+        do {
+            dz4 = 0;
+            System.out.print("Ведите номер ДЗ 4: ");
+            if (sc.hasNextInt()) {
+
+
+                dz4 = sc.nextInt();
+                switch (dz4) {
+                    case (0):
+                        System.out.println("Домашнее задание №0");
+                        dz04_00();
+                        //окончание?
+                        break;
+                    case (1):
+                        System.out.println("Домашнее задание №1");
+                        dz04_01();
+                        //Ввели не номер задания..
+                        break;
+                    case (2):
+                        System.out.println("Домашнее задание №2");
+                        dz04_02();
+                        //Ввели не номер задания..
+                        break;
+                    case (3):
+                        System.out.println("Домашнее задание №3");
+                        dz04_03();
+                        //Отлично!
+                        break;
+                    case (4):
+//                    Дополнительное задание
+                        System.out.println("Дополниетльное Домашнее задание №4");
+                        dz04_04();
+                        //Отлично!
+                        break;
+                    case (5):
+                        dz04_05();
+                        //Отлично!
+                        break;
+                    default:
+                        System.out.println("нет такого Домашнего Задания!");
+                }
+
+            } else {
+                System.out.println("Вы ввели не номер задания...");
+                break;
+            }
+        } while (dz4 < 7);
+        sc.close();
+    }
+
+    private static void dz04_05() {
+//        Создайте массив и заполните массив.
+//        Выведите массив на экран в строку.
+//        Замените каждый элемент с нечётным индексом на ноль.
+//        Снова выведете массив на экран на отдельной строке.
+
+        int[] a = new int[10];
+        int[] b = new int[10];
+        System.out.print("Произволный исходный массив : { ");
+        for (int i = 0; i < a.length; i++) {
+            a[i] = (int) (Math.random() * 10);
+            System.out.print(a[i] + " ");
+            if (!(a[i] % 2 == 0)) {
+                b[i] = 0;
+            } else {
+                b[i] = (int) a[i];
             }
         }
-        sc.close();
+        System.out.println("}");
+        System.out.print("Измененный массив : { ");
+        for (int i = 0; i < b.length; i++) {
+            System.out.print(b[i] + " ");
+        }
+        System.out.println("}\n");
+    }
+
+    private static void dz04_04() {
+//        Создайте массив из n случайных целых чисел и выведите его на экран.
+//         Размер массива пусть задается с консоли и размер массива может быть
+//         больше 5 и меньше или равно 10.
+//        Если n не удовлетворяет условию - выведите сообщение об этом.
+//        Если пользователь ввёл не подходящее число, то программа должна
+//         просить пользователя повторить ввод.
+//        Создайте второй массив только из чётных элементов первого массива,
+//                если они там есть, и вывести его на экран.
+        Scanner sc = new Scanner(System.in);
+        Integer size = null;
+        Integer size2 = null;
+//        Random rnd = new Random();
+
+        do {
+            System.out.print("Задайте размер массива (только >5 и <=10) : ");
+            size = 0;
+            size2 = 0;
+            if (sc.hasNextInt()) {
+                size = sc.nextInt();
+                if ((size <= 5) || (size > 10)) {
+                    System.out.println("Недопустимый размер массива.");
+                    continue;
+                }
+
+                int[] a = new int[size];
+                System.out.print("Эллементы массива 1: ");
+                for (int i = 0; i < a.length; i++) {
+//                    Можно так, но неудобно смотреть.
+//                    a[i] = rnd.nextInt();
+                    a[i] = (int) (Math.random() * 10);
+                    System.out.print(a[i] + " ");
+
+//                    Вычисляем размер массива 2
+                    if ((a[i] % 2 == 0) && (a[i] != 0)) {
+                        size2++;
+                    }
+                }
+
+                System.out.println("\nРазмер массива 2 : " + size2);
+                int[] b = new int[size2];
+                System.out.print("Эллементы массива 2: ");
+                for (int i = 0, j = 0; i < a.length; i++) {
+                    if ((a[i] != 0) && (a[i] % 2 == 0)) {
+                        b[j] = (int) a[i];
+                        System.out.print(b[j] + " ");
+                        j++;
+                    }
+                }
+
+            } else {
+                System.out.println("Введено не число!");
+                break;
+            }
+        } while ((size > 10) || (size <= 5));
+        System.out.println("\n");
     }
 
     private static void dz04_03() {
@@ -32,6 +151,40 @@ public class Lesson_4 {
 //        Посчитайте среднее арифметическое элементов каждого массива и
 //        сообщите, для какого из массивов это значение оказалось больше (либо
 //        сообщите, что их средние арифметические равны).
+
+        int[] a = {9, 5, 3, 5, 7};
+        int[] b = {7, 8, 4, 5, 5};
+        double a_sr = 0, b_sr = 0;
+        int ab = 0, bb = 0;
+
+        System.out.print("Массив 1 : { ");
+        for (int i = 0; i < a.length; i++) {
+            a_sr += a[i];
+            System.out.print(a[i] + " ");
+        }
+        a_sr = (a_sr / a.length);
+        System.out.println("}\nСреднее Арифметическое массива 1 : " + a_sr);
+
+        System.out.print("Массив 2 : { ");
+        for (int i = 0; i < b.length; i++) {
+            b_sr += b[i];
+            System.out.print(b[i] + " ");
+        }
+        b_sr = (b_sr / b.length);
+        System.out.println("}\nСреднее Арифметическое массива 2 : " + b_sr);
+
+//        Приверка массива 1 и 2, какой больше или равны
+        if (a_sr >= b_sr) {
+            if (a_sr > b_sr) System.out.println("Среднее арифметической Массива №1 больше №2");
+            else System.out.println("Среднее арифметической Массива №1 равно №2");
+        } else System.out.println("Среднее арифметической Массива №2 больше №1");
+        System.out.println();
+//        Можно и так вывести массив на экран
+//        System.out.println("Первый массив: {" + a[0] + ", " + a[1] + ", " + a[2]
+//                + ", " + a[3] + ", " + a[4] + "}");
+//        System.out.println("Второй массив: {" + b[0] + ", " + b[1] + ", " + b[2]
+//                + ", " + b[3] + ", " + b[4] + "}");
+//        Высчитать среднее арифметическое.
     }
 
     private static void dz04_02() {
@@ -119,6 +272,7 @@ public class Lesson_4 {
             } else System.out.println("Числа нет в массиве!");
         } else System.out.println("Ввели не число!");
         sc.close();
+        System.out.println();
     }
 
     private static void dz04_00() {
